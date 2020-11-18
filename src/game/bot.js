17090dc404db36f1b333aa9b3f9eaa9db69b4cca
d9nich <dan.galaiko@gmail.com>
2020-11-18 20:44:29 +0200

    Simple logic of bot has been writen

--- src/game/bot.js
+++ src/game/bot.js
@@ -1,3 +1,23 @@
 export function getChoice(gameState) {
-    return gameState.availableCoefficients()[0];
+    var availableCoefficients = gameState.availableCoefficients();
+    var currentPoints = gameState.firstPlayer.totalPoints;
+    var totalOfDices = gameState.firstDice + gameState.secondDice;
+    var bestCoefficient = availableCoefficients[0];
+    var bestAmount = getCoefficientHeuristic(bestCoefficient, currentPoints, totalOfDices);
+    for (let coefficient of availableCoefficients) {
+        let currentCoefficientAmount = getCoefficientHeuristic(coefficient, currentPoints, totalOfDices);
+        if (bestAmount < currentCoefficientAmount) {
+            bestAmount = currentCoefficientAmount;
+            bestCoefficient = coefficient;
+        }
+    }
+    return bestCoefficient;
+}
+
+function getCoefficientHeuristic(coefficient, currentPoints, diceAmount) {
+    var total = currentPoints + diceAmount * coefficient;
+    if (total % 13 === 0) {
+        return total / 13;
+    }
+    return 0;
 }
