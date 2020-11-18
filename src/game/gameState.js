1fd06b21928cb0042a21720efafc068253318b7b
d9nich <dan.galaiko@gmail.com>
2020-11-18 15:27:31 +0200

    Added gameState.js

--- src/game/gameState.js
+++ src/game/gameState.js
@@ -1,2 +1,35 @@
+import {UserState, generateThrowOfFirstDice, generateThrowOfSecondDice} from "./userState";
+
 export class GameState {
+    firstPlayer = new UserState();
+    secondPlayer = new UserState();
+
+    isEnd() {
+        return this.firstPlayer.isEnd() && this.secondPlayer.isEnd();
+    }
+
+    firstDice;
+    secondDice;
+
+    isRolled = false;
+
+    rollDice() {
+        this.firstDice = generateThrowOfFirstDice();
+        this.secondDice = generateThrowOfSecondDice();
+        this.isRolled = true;
+    }
+
+    makeTurn(coefficient) {
+        if (this.isRolled) {
+            if (!this.firstPlayer.isEnd()) {
+                this.firstPlayer.makeTurn(this.firstDice, this.secondDice, coefficient);
+            } else {
+                this.firstPlayer.makeTurn(this.firstDice, this.secondDice, coefficient);
+            }
+        }
+    }
+
+    whoWon() {
+        return this.firstPlayer.winScores - this.secondPlayer.winScores;
+    }
 }
